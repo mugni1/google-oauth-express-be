f4a7ce9b59f1cdd96b7a97bcec36be51258dacb8
mugni1 <asepabdulmugni22@gmail.com>
2025-11-19 20:42:39 +0700

    feat: add root path

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import express from "express";
-import authRouter from "@/routes/auth.routes";
-  
+import express, { Request, Response } from "express";
+import authRouter from "./src/routes/auth.routes";
+
 const app = express()
+app.get("/", (req: Request, res: Response) => { res.json({ message: "Miau Miau" }) })
 app.use(express.json())
 app.use(authRouter)
 export default app;
