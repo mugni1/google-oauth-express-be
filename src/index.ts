28378876718f436c169b7d8ba0e6a27c5165fae8
mugni1 <asepabdulmugni22@gmail.com>
2025-11-19 21:17:53 +0700

    anu

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const app = express()
 app.get("/", (req: Request, res: Response) => { res.json({ message: "Miau Miau" }) })
 app.use(express.json())
 app.use(authRouter)
-
+app.listen(5050, () => console.log("Server running on port 5050"))
 export default app;
