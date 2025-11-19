d860b522b5c914ca93b831e9eb9e84496c9d6cfa
mugni1 <asepabdulmugni22@gmail.com>
2025-11-19 23:22:24 +0700

    remove: port

--- dist/index.js
+++ dist/index.js
@@ -5,5 +5,4 @@ app.use(express.json());
 app.get("/", (req, res) => { res.json({ message: "Miau Miau" }); });
 app.use(authRouter);
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app;
