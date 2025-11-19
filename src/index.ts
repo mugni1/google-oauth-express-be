40ec2ff83068587d18e0a00e23d4ba4ae021f68a
mugni1 <asepabdulmugni22@gmail.com>
2025-11-19 22:47:58 +0700

    chore: improve

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express, { Request, Response } from "express";
 import authRouter from "./routes/auth.routes";
 
 const app = express()
-app.get("/", (req: Request, res: Response) => { res.json({ message: "Miau Miau" }) })
 app.use(express.json())
+
+app.get("/", (req: Request, res: Response) => { res.json({ message: "Miau Miau" }) })
 app.use(authRouter)
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app;
