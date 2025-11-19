c38cb2f09f057593402ed24a6e7e2d67fe95e365
mugni1 <asepabdulmugni22@gmail.com>
2025-11-19 21:30:26 +0700

    anu

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ const app = express()
 app.get("/", (req: Request, res: Response) => { res.json({ message: "Miau Miau" }) })
 app.use(express.json())
 app.use(authRouter)
-app.listen(5050, () => console.log("Server running on port 5050"))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 export default app;
